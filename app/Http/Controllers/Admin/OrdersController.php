ac1e586ca5ab4fefa7ec68661dac36e9304ce89e
stands30 <lancylotdsouza@gmail.com>
2022-01-08 17:23:29 +0530

    order dashboard created

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\OrdersModel;
 
 class OrdersController extends Controller
 {
 
     public function index_page(){
-        return view('admin.orders');
+        $orders = OrdersModel::join('order_details', 'order_details.order_id', 'orders.id')->orderBy('orders.id', 'desc')->get();
+        return view('admin.orders', compact('orders'));
     }
 }
