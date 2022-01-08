a558ca2cb89b7b5c6293ebeb926c3d4df7072d91
stands30 <lancylotdsouza@gmail.com>
2022-01-08 14:38:31 +0530

    new sales added

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
     }
 
     public function adminDashboard(){
-        $data['new_sales'] = OrdersModel::select(DB::raw('COUNT(id) as new_sales'))->where('status', 1)->first()->new_sales;
+        $data['new_sales'] = OrdersModel::select(DB::raw('COUNT(id) as new_sales'))->whereRaw('created_at > now() - interval 1 hour')->where('status', 1)->first()->new_sales;
         $data['pending_orders'] = OrdersModel::select(DB::raw('COUNT(id) as pending_orders'))->where('status', 2)->first()->pending_orders;
         $data['completed_orders'] = OrdersModel::select(DB::raw('COUNT(id) as completed_orders'))->where('status', 3)->first()->completed_orders;
         $data['daily_sales'] = OrdersModel::select(DB::raw('SUM(total_amount) as daily_sales'))->where('created_at', date('Y-m-d'))->first()->daily_sales;
